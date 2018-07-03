316dcb20b964edc9557ea1dafc91625fab3f6101
Vince <vincentfiorilli@gmail.com>
2018-07-02 20:31:43 -0400

    switch statement and module

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const keys = require('./keys.js')
+//console.log('keys ' + JSON.stringify(keys.twitterKeys, null, 2))
 
-console.log('keys ' + JSON.stringify(keys.twitterKeys, null, 2))
+var command = process.argv[2]
+
+switch(command){
+  case `my-tweets`:
+    console.log('tweet bird')
+    break;
+  case `spotify-this-song`:
+    console.log('spotify fool')
+    break;
+  case `movie-this`:
+    console.log('this is movie info')
+    break;
+  case `do-what-it-says`:
+    console.log('DO WHAT I SAY!!!')
+    break;
+}
