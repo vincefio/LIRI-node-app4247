40bc1d81c5d9bd39ea69b732362f66fc88f595fc
Vince <vincentfiorilli@gmail.com>
2018-07-02 22:47:04 -0400

    dont know how this is working

--- app.js
+++ app.js
@@ -26,23 +26,34 @@ switch(command){
 
 
 //query twitter api with request
-request('https://api.twitter.com/1.1/statuses/user_timeline.json?user_id=crimshawrocks&count=5', function (error, response, body) {
+/*request('https://api.twitter.com/1.1/statuses/user_timeline.json?user_id=crimshawrocks&count=5', function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
-});
+});*/
 
-var client = new Twitter({
-  consumer_key: keys.consumer_key,
-  consumer_secret: keys.consumer_secret,
-  access_token_key: keys.access_token_key,
-  access_token_secret: keys.access_token_secret
-});
+var client = new Twitter(keys.twitterKeys);
 //40e9cece
 //http://www.omdbapi.com/?apikey=40e9cece&t=titanic
-var params = {screen_name: 'crimshawrocks'};
+//var params = {screen_name: 'crimshawrocks'};
 client.get('http://www.omdbapi.com/?apikey=40e9cece&t=titanic', function(error, tweets, response) {
   if (!error) {
     console.log(tweets);
+  }
+});
+
+
+
+var params = {
+  screen_name: "crimshawrocks"
+};
+client.get("statuses/user_timeline", params, function(error, tweets, response) {
+  if (!error) {
+  for (var i = 0; i < tweets.length; i++) {
+      console.log(tweets[i].created_at);
+      console.log("");
+      console.log(tweets[i].text);
+    }
+    //console.log('response ' + tweets.length)//JSON.stringify(tweets))
   }
 });
